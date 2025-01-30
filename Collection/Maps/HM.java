e90a1c99a4238b1d6cb55e4ccec7aadfa5da71ce
PavanKumarB <bollupavankumar12@gmail.com>
2025-01-30 10:57:05 +0530

    Updated the Collection Notes With Some Programs

---
+++ Collection/Maps/HM.java
@@ -0,0 +1,7 @@
+class  HM
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("HashMap");
+	}
+}
---
+++ Collection/Maps/LHM.java
@@ -0,0 +1,7 @@
+class LHM
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
